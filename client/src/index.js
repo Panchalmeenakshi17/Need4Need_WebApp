2f7309e19dd5489ad4b3e8ad78a060b65185f020
Meenakshi <157927822+Panchalmeenakshi17@users.noreply.github.com>
2024-08-05 14:58:54 +0530

    e

--- client/src/index.js
+++ client/src/index.js
@@ -4,18 +4,18 @@ import App from "./App";
 import "./index.css";
 import { Provider } from "react-redux";
 import store from "./store";
-import { Auth0Provider } from "@auth0/auth0-react";
+// import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Auth0Provider
-    domain="dev-b1jgfzu5x1tvle0v.us.auth0.com"
-    clientId="fzfT4iMBJ9X9V3FGFV3wlzZcCz0pLJBJ"
-    authorizationParams={{
-      redirect_uri: window.location.origin,
-    }}
-  >
+  // <Auth0Provider
+  //   domain="dev-b1jgfzu5x1tvle0v.us.auth0.com"
+  //   clientId="fzfT4iMBJ9X9V3FGFV3wlzZcCz0pLJBJ"
+  //   authorizationParams={{
+  //     redirect_uri: window.location.origin,
+  //   }}
+  // >
     <Provider store={store}>
       <App />
     </Provider>
-  </Auth0Provider>
+  // </Auth0Provider>
 );
